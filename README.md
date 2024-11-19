b231ebd9201bd982567a9a29814e21f34bf092f3
Roshini vemuluri <prasadvemuluri713@gmail.com>
2024-11-19 22:14:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Roshini-data-analytics-training" 
---
+++ permutation.py
@@ -0,0 +1,10 @@
+import itertools
+
+# Define a set of objects
+objects = ['A', 'B', 'C']
+
+# Calculate all permutations of 3 objects from the set
+permutations = list(itertools.permutations(objects))
+
+# Display the permutations
+print("Permutations of the set:", permutations)
