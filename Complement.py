fe886fa5d5d09252aa85a7733a66f1662edfe6c0
Roshini vemuluri <prasadvemuluri713@gmail.com>
2024-11-20 14:17:36 +0530

    Add files via upload

---
+++ Complement.py
@@ -0,0 +1,10 @@
+# Define the universal set (all possible outcomes)
+U = {1, 2, 3, 4, 5, 6}
+
+# Define the event set A
+A = {1, 2, 3}
+
+# Complement of A: All elements in U that are not in A
+complement_A = U.difference(A)
+
+print("Complement of A:", complement_A)
