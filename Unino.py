e11d7a86e82f4c5e1415333c3158789ec60865d8
Roshini vemuluri <prasadvemuluri713@gmail.com>
2024-11-20 14:16:27 +0530

    Add files via upload

---
+++ Unino.py
@@ -0,0 +1,8 @@
+# Define two sets representing events A and B
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+# Union: All elements in A or B (or both)
+union_set = A.union(B)
+
+print("Union of A and B:", union_set)
